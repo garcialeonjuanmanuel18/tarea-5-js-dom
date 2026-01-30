8d01cf99a133f6ee7460b6a1f584e02d2416b9f4
Juan Manuel García León <garcialeonjuanmanuel@gmail.com>
2026-01-30 15:54:54 +0100

    hsfkjhskh

---
+++ 1.js
@@ -0,0 +1,11 @@
+const titulo = document.getElementById("titulo");
+const contenedor = document.getElementById("contenedor");
+const parrafo = document.getElementById("parrafo");
+const boton = document.getElementById("boton");
+const infocontacto = document.getElementById("infocontacto");
+
+console.log(titulo);
+console.log(contenedor);
+console.log(parrafo);
+console.log(boton);
+console.log(infocontacto);
